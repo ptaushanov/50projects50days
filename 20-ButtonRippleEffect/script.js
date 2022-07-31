2cdd8e2fee400e9019fe634fbd844e162816864c
ptaushanov <pepi15032000@gmail.com>
2022-07-31 19:30:27 +0300

    fix: changes to project 20

--- 20-ButtonRippleEffect/script.js
+++ 20-ButtonRippleEffect/script.js
@@ -2,10 +2,18 @@ const buttons = document.querySelectorAll(".ripple")
 
 buttons.forEach(button => {
     button.addEventListener("click", (event) => {
-        const { layerX, layerY, target: { innerText } } = event
-        event.target.innerHTML = `
-        ${innerText}
-        <span class="circle" style="top: ${layerY}px; left: ${layerX}px;"></span>
-    `
+        console.log("click")
+        const { layerX, layerY, target } = event
+        createRipple(target, layerX, layerY)
     })
 })
+
+function createRipple(target, x, y) {
+    const ripple = document.createElement("span")
+    ripple.classList.add("circle")
+    ripple.style.top = y + "px"
+    ripple.style.left = x + "px"
+
+    target.appendChild(ripple)
+    setTimeout(() => { ripple.remove() }, 1000)
+}
